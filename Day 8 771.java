944672c57f824d29c40a7c0b1659b3ded5199832
mhdaadil279 <mhdaadil279@gmail.com>
2026-07-23 09:40:00 +0530

    Create Day 8 771.java

---
+++ Day 8 771.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set = new HashSet<>();
+
+        // Store all jewels
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+
+        int count = 0;
+
+        // Count matching stones
+        for (char c : stones.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
